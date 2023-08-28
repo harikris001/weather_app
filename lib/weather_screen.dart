878804fbe9c50b739043dca604914a7795ce2174
harikris001 <hk5983835@gmail.com>
2023-08-28 21:19:14 +0530

    added placeholder widgets

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -6,38 +6,67 @@ class WeatherScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Weather App",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.refresh,
-              size: 30,
+        appBar: AppBar(
+          title: const Text(
+            "Weather App",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
             ),
-            tooltip: 'Refresh',
-          )
-        ],
-      ),
-      body: const Column(
-        children: [
-          Card(
-            child: Text(
-              '30 °C',
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.refresh,
+                size: 30,
+              ),
+              tooltip: 'Refresh',
+            )
+          ],
+        ),
+        body: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Placeholder(
+              fallbackHeight: 140,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Text(
+              'Weather Forecast',
               style: TextStyle(
-                fontSize: 32,
                 fontWeight: FontWeight.bold,
+                fontSize: 18,
               ),
             ),
-          ),
-        ],
-      ),
-    );
+            SizedBox(
+              height: 10,
+            ),
+            Placeholder(
+              fallbackHeight: 120,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Weather Information',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [],
+            ),
+          ],
+        ));
   }
 }
